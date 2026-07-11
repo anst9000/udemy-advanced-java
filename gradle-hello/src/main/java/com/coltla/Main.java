c0c93f5fc3e266856e5e730642f405d4f8d3348d
Anders Strömberg <coltlaboy@yahoo.se>
2026-07-11 21:14:11 +0200

    created

---
+++ gradle-hello/src/main/java/com/coltla/Main.java
@@ -0,0 +1,11 @@
+package com.coltla;
+
+public class Main {
+    public static void main(String[] args) {
+        // Change this line:
+        System.out.println(String.format("Hello and welcome!"));
+
+        // Note: You can also simplify it to just this:
+        // System.out.println("Hello and welcome!");
+    }
+}
